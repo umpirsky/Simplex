004b2b6864299b6f81bb65bc7eb42e8c2a12ece1
Sasa Stamenkovic <umpirsky@gmail.com>
2012-02-18 13:01:17 +0100

    Fixed autoloader.

--- autoload.php
+++ autoload.php
@@ -9,11 +9,19 @@
 * file that was distributed with this source code.
 */
 
-require __DIR__ . '/vendor/silex/autoload.php';
+if (false === class_exists('Symfony\Component\ClassLoader\UniversalClassLoader', false)) {
+    require_once __DIR__ . '/vendor/silex/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+}
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Simplex' => __DIR__ . '/src',
+    'Symfony'   => __DIR__ . '/vendor/silex/vendor',
+    'Silex'     => __DIR__ . '/vendor/silex/src',
+    'Simplex'   => __DIR__ . '/src',
 ));
+$loader->registerPrefixes(array(
+    'Pimple' => __DIR__ . '/vendor/silex/vendor/pimple/lib',
+));
+$loader->register();
