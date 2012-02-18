be089f7e68154c5c9a574dce10a1f3aec23e0c2f
Sasa Stamenkovic <umpirsky@gmail.com>
2012-02-18 10:55:12 +0100

    Added Application.

---
+++ src/Simplex/Application.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+* This file is part of the Simplex framework.
+*
+* (c) Саша Стаменковић <umpirsky@gmail.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Simplex;
+
+/**
+ * The Simplex framework class.
+ *
+ * @author Саша Стаменковић <umpirsky@gmail.com>
+ */
+class Application extends \Silex\Application {
+}
