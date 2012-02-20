79244cdb92e9a46dd55985a513c3c4a20dfe274b
umpirsky <umpirsky@gmail.com>
2012-02-20 10:37:26 +0100

    Moved registering providers to website.

--- src/Simplex/Application.php
+++ src/Simplex/Application.php
@@ -17,45 +17,18 @@ namespace Simplex;
  * @author Саша Стаменковић <umpirsky@gmail.com>
  */
 class Application extends \Silex\Application {
-    
+
     /**
      * Application constructor.
-     * 
+     *
      * @param array $options
      */
     public function __construct(array $options) {
 
-        $app = $this;
-        
         parent::__construct();
-        
-        if (!isset($options['simplex.application_path'])) {
-            $options['simplex.application_path'] = __DIR__ . '/../../../../';
-        }
-        
-        // Register extensions
-        $this->register(new \Silex\Provider\TwigServiceProvider(), array(
-            'twig.path' => $options['simplex.application_path'] . $options['twig.path'],
-            'twig.class_path' => __DIR__ . '/../../vendor/silex/vendor/twig/lib'
-        ));
-        $this->register(new \Silex\Provider\UrlGeneratorServiceProvider());
-        $this->register(new \Simplex\Provider\PageServiceProvider(), array(
-            'navigation' => $options['navigation']
-        ));
-        $this->register(new \Simplex\Provider\NavigationServiceProvider(), array(
-            'navigation' => $options['navigation']
-        ));
-        
-        // Register error handlers
-        $this->error(
-            function (\Exception $e) use ($app) {
-                if ($e instanceof NotFoundHttpException) {
-                    return $app['twig']->render('error.html.twig', array('code' => 404));
-                }
 
-                $code = ($e instanceof HttpException) ? $e->getStatusCode() : 500;
-                return $app['twig']->render('error.html.twig', array('code' => $code));
-            }
-        );
+        foreach ($options as $k => $v) {
+            $app[$k] = $v;
+        }
     }
 }
